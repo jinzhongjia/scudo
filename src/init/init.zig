8a92841484333bf7f3ded410a347c0efd9dc8d46
jinzhongjia <jinzhongjia@qq.com>
2023-04-27 16:20:24 +0800

    Update: now we can support multiboot2

--- src/init/init.zig
+++ src/init/init.zig
@@ -1,18 +1,19 @@
 const std = @import("std");
-const multiboot = @import("multiboot").v1;
+const multiboot_v1 = @import("multiboot").v1;
+// const multiboot_v2 = @import("multiboot").v2;
 const assembly = @import("cpu").x86.assembly;
 
 // NOTE: assert should be replace with @function panic implemented by us.
 const assert = std.debug.assert;
 
-export const header_v1 linksection(".multiboot_v1") = multiboot.Header{};
+export const header_v1 linksection(".multiboot_v1") = multiboot_v1.Header{};
 
-// export const header_v2 linksection(".multiboot_v2") = multiboot.v2.Header{};
+// export const header_v2 linksection(".multiboot_v2") = multiboot_v2.Header{};
 
-export fn init(magic: u32, info: *const multiboot.Info) void {
+export fn init(magic: u32, info: *const multiboot_v1.Info) void {
     _ = info;
 
-    assert(magic == multiboot.BOOT_MAGIC);
+    assert(magic == multiboot_v1.BOOT_MAGIC);
 
     // assembly.sti();
     assembly.hlt();
