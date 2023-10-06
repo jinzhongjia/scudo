a13870832e3b0aeca4cbfd7ba3bac2817027955e
jinzhongjia <mail@nvimer.org>
2023-10-06 20:07:35 +0800

    clean up code

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -1,15 +1,7 @@
-const limine = @import("limine");
-const std = @import("std");
-
 const lib = @import("lib.zig");
-const tty = lib.tty;
 
-// 我们内核的入口函数，通过ld连接脚本告诉来了limine,入口在这
-export fn _start() callconv(.C) noreturn {
-
-    // 初始化tty的工作
-    // TODO: print boot time
-    tty.init();
+pub fn main() noreturn {
+    lib.tty.init();
     lib.idt.init();
     lib.clock.init();
     lib.sound.init();
@@ -18,8 +10,3 @@ export fn _start() callconv(.C) noreturn {
 
     @panic("Note:This is an experimental project!\nWe're done, just hang...");
 }
-
-var time: u64 = 0;
-
-// 在root作用域定义一个pub panic,覆盖默认的panic
-pub const panic = tty.panic;
