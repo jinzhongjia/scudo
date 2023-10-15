bb93a91a70d037aaae3a96625418f36f9d7cc403
jinzhongjia <mail@nvimer.org>
2023-10-15 20:32:14 +0800

    add cpuid instruction support

--- kernel/src/cpu.zig
+++ kernel/src/cpu.zig
@@ -65,3 +65,33 @@ pub inline fn invlpg(v_addr: usize) void {
         : "memory"
     );
 }
+
+const CPUID = extern struct {
+    eax: u32,
+    ebx: u32,
+    edx: u32,
+    ecx: u32,
+};
+
+pub inline fn cpuid(leaf: u32) CPUID {
+    var eax: u32 = undefined;
+    var ebx: u32 = undefined;
+    var edx: u32 = undefined;
+    var ecx: u32 = undefined;
+
+    asm volatile (
+        \\cpuid
+        : [eax] "={eax}" (eax),
+          [ebx] "={ebx}" (ebx),
+          [edx] "={edx}" (edx),
+          [ecx] "={ecx}" (ecx),
+        : [leaf] "{eax}" (leaf),
+    );
+
+    return CPUID{
+        .eax = eax,
+        .ebx = ebx,
+        .edx = edx,
+        .ecx = ecx,
+    };
+}
