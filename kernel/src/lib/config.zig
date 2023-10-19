0e1da0c6bd690d3510edb69db004ded750e5757f
jinzhongjia <mail@nvimer.org>
2023-10-19 21:17:28 +0800

    add config for memory print

--- kernel/src/lib/config.zig
+++ kernel/src/lib/config.zig
@@ -6,3 +6,16 @@ pub const PIT_FREQUENCY = 100;
 
 /// this will control whether init the PC_SPEAKER
 pub const enable_PC_SPEAKER: bool = true;
+
+pub const mem = struct {
+    pub const is_print_mem_info: bool = true;
+
+    /// display type: 0 is B, 1 is KB, 2 is MB, 3 is GB
+    pub const display_type: u8 = 3;
+
+    comptime {
+        if (display_type > 3) {
+            @panic("display_type must be less than 4");
+        }
+    }
+};
