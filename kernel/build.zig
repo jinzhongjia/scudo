41eee68cd31951a2ec33ae9be5c7519f4941a82f
jinzhongjia <mail@nvimer.org>
2023-10-20 21:05:47 +0800

    update build.zig

--- kernel/build.zig
+++ kernel/build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
+
+pub fn build(b: *std.Build) !void {
     // Define a freestanding x86_64 cross-compilation target.
     var target: std.zig.CrossTarget = .{
         .cpu_arch = .x86_64,
@@ -28,6 +29,7 @@ pub fn build(b: *std.build.Builder) !void {
         .optimize = optimize,
     });
     kernel.code_model = .kernel;
+
     kernel.addModule("limine", limine.module("limine"));
     kernel.setLinkerScriptPath(.{ .path = "linker.ld" });
     kernel.pie = true;
