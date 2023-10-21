98e85bfabd019ffd1995ab9d8dfdd7859c404302
jinzhongjia <mail@nvimer.org>
2023-10-21 13:25:32 +0800

    add assembly function to get max physical address

--- kernel/src/cpu.zig
+++ kernel/src/cpu.zig
@@ -71,33 +71,39 @@ const CPUID = extern struct {
     ebx: u32,
     edx: u32,
     ecx: u32,
+
+    /// get max physical address
+    pub inline fn get_max_physical_address() u6 {
+        return @truncate(cpuid(0x80000008).eax);
+    }
+
+    // NOTE:the MAXPHYADDR which is gaven by bochs and qemu is 40, that's a terrabyt.
+    //
+    //maybe we need more to write to the address
+
+    pub inline fn cpuid(leaf: u32) CPUID {
+        var eax: u32 = undefined;
+        var ebx: u32 = undefined;
+        var edx: u32 = undefined;
+        var ecx: u32 = undefined;
+
+        asm volatile (
+            \\cpuid
+            : [eax] "={eax}" (eax),
+              [ebx] "={ebx}" (ebx),
+              [edx] "={edx}" (edx),
+              [ecx] "={ecx}" (ecx),
+            : [leaf] "{eax}" (leaf),
+        );
+
+        return CPUID{
+            .eax = eax,
+            .ebx = ebx,
+            .edx = edx,
+            .ecx = ecx,
+        };
+    }
 };
-
-// NOTE:the MAXPHYADDR which is gaven by bochs and qemu is 40, that's a terrabyt.
-//maybe we need more to write to the address
-
-pub inline fn cpuid(leaf: u32) CPUID {
-    var eax: u32 = undefined;
-    var ebx: u32 = undefined;
-    var edx: u32 = undefined;
-    var ecx: u32 = undefined;
-
-    asm volatile (
-        \\cpuid
-        : [eax] "={eax}" (eax),
-          [ebx] "={ebx}" (ebx),
-          [edx] "={edx}" (edx),
-          [ecx] "={ecx}" (ecx),
-        : [leaf] "{eax}" (leaf),
-    );
-
-    return CPUID{
-        .eax = eax,
-        .ebx = ebx,
-        .edx = edx,
-        .ecx = ecx,
-    };
-}
 
 /// read cr2 register
 pub inline fn readCR2() usize {
